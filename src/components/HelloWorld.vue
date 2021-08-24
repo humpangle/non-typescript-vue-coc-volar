36d29889a43d7406a803929854f75a794fda93b3
Kanmii <humpangle@gmail.com>
2021-08-24 03:33:19 +0200

    inital commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div @click="sayHi">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  methods: {
+    sayHi() {
+      console.log("hi");
+    },
+  },
+};
+</script>
